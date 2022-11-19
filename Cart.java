b7bdf269476a691735d2996a9bda7024acc08f60
ericnem <ericnem@outlook.com>
2022-11-19 17:52:54 -0500

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -9,9 +9,18 @@ public class Cart
   public void show(JFrame f)
   {
     f.setSize(450,900); 
-    f.getContentPane().setBackground(new java.awt.Color(122, 143, 222));   
-    f.setLayout(null);    
+    f.getContentPane().setBackground(new java.awt.Color(122, 143, 222)); 
+
+    JPanel mainframe = new JPanel();
+    mainframe.setLayout(new BoxLayout(mainframe,1));
+    mainframe.add(new JLabel("Lol"));
+    mainframe.add(new JLabel("Lolly"));
+    mainframe.setPreferredSize(new Dimension( 450,2000));
+    mainframe.setBackground(new java.awt.Color(122, 143, 222));
+    JScrollPane scrollFrame = new JScrollPane(mainframe);
+    scrollFrame.setPreferredSize(new Dimension( 450,2000));
+    f.add(scrollFrame); 
     f.setVisible(true); 
     f.setResizable(false);
-    }
+  }
 }
