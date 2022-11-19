76b096e838fd3e59da631ab3d1cc8f4654a7de29
ericnem <ericnem@outlook.com>
2022-11-19 17:08:13 -0500

    App Frame
    Adds coloured App Frame

--- Cart.java
+++ Cart.java
@@ -1,1 +1,17 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.event.*;
+import java.awt.Font;
+import java.awt.*;
 
+public class Cart
+{
+  public void show(JFrame f)
+  {
+    f.setSize(450,900); 
+    f.getContentPane().setBackground(new java.awt.Color(122, 143, 222));   
+    f.setLayout(null);    
+    f.setVisible(true); 
+    f.setResizable(false);
+    }
+}
