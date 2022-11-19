45e339503c78188636495b5e740da7b6cfe965a8
Yarik-Popov <yarikpopov03@gmail.com>
2022-11-19 18:56:06 -0500

    Fixed Formatting

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
-public class Main {
-    public static void main(String[] args) 
-    {
+import javax.swing.*;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    // Login screen
+    JFrame f=new JFrame("Fridgy");
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //Initiates login window
+    Cart newWindow = new Cart();
+    newWindow.show(f);
     }
 }
+
