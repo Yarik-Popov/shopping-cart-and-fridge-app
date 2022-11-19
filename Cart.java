6f649e9106b7dfc9f5d9b3fda70d5fe707f50990
ericnem <ericnem@outlook.com>
2022-11-19 17:55:35 -0500

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -13,12 +13,15 @@ public class Cart
 
     JPanel mainframe = new JPanel();
     mainframe.setLayout(new BoxLayout(mainframe,1));
-    mainframe.add(new JLabel("Lol"));
-    mainframe.add(new JLabel("Lolly"));
     mainframe.setPreferredSize(new Dimension( 450,2000));
     mainframe.setBackground(new java.awt.Color(122, 143, 222));
+    
     JScrollPane scrollFrame = new JScrollPane(mainframe);
     scrollFrame.setPreferredSize(new Dimension( 450,2000));
+    scrollFrame.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // Disables horizontal scrolling
+    scrollFrame.getVerticalScrollBar().setUnitIncrement(10);
+    scrollFrame.getVerticalScrollBar().setPreferredSize(new Dimension(0,0)); // Makes vertical scrollbar's dimensions 0 to make it invisible
+    
     f.add(scrollFrame); 
     f.setVisible(true); 
     f.setResizable(false);
