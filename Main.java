e9f61058bcd6805da93b4a580b7cbf6ede26f7cf
Yarik-Popov <yarikpopov03@gmail.com>
2022-11-19 20:42:24 -0500

    Fridge Text is correct

--- Main.java
+++ Main.java
@@ -3,11 +3,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         // Login screen
-        JFrame f=new JFrame("Fridgy");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // JFrame f=new JFrame("Fridgy");
+        // f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
         //Initiates login window
-        Cart newWindow = new Cart();
-        newWindow.show(f);
+        Fridge newWindow = new Fridge();
+        newWindow.add("apple");
+        newWindow.add("banana");
+        newWindow.add("pear");
+        newWindow.commitItems();
+        // newWindow.show(f);
     }
 }
