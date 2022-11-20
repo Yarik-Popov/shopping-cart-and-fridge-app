21b58f00b73fa7ddb33544ac7403cd44c063f6de
Yarik-Popov <yarikpopov03@gmail.com>
2022-11-19 19:52:25 -0500

    Change the Main file

--- MAIN.java
+++ MAIN.java
@@ -1,15 +1,12 @@
-import javax.swing.*;
-
-public class main {
+public class Main {
     public static void main(String[] args) throws Exception {
 
-    // Login screen
-    JFrame f=new JFrame("Fridgy");
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    //Initiates login window
-    Cart newWindow = new Cart();
-    newWindow.show(f);
+        // Login screen
+        JFrame f=new JFrame("Fridgy");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+        //Initiates login window
+        Cart newWindow = new Cart();
+        newWindow.show(f);
     }
 }
-
--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) throws Exception {
 
+        // Login screen
+        JFrame f=new JFrame("Fridgy");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+        //Initiates login window
+        Cart newWindow = new Cart();
+        newWindow.show(f);
     }
 }
