c2de96d03cc20a377bd9c727017764a1c3d746ad
JaydenNikifork <jayden.nikifork@gmail.com>
2022-11-19 19:37:09 -0500

    NewCode

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-public class main {
+public class Main {
     public static void main(String[] args) throws Exception {
 
         // Login screen
@@ -11,29 +11,32 @@ public class main {
         JButton switchTabs = new JButton("Switch Tabs");
         //f.add(switchTabs);
         JScrollPane fridgePane = fridge.show(f);
-        JScrollPane cartPane = fridge.show(f);
+        // JScrollPane cartPane = fridge.show(f);
+        // f.remove(cartPane);
+        // f.repaint();
+        // f.revalidate();
         
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Initiates login window
         //Cart newWindow = new Cart();
         //newWindow.show(f);
         switchTabs.setBounds(0, 0, 450, 50);
-        boolean cart = false;
-        switchTabs.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (cart) {
-                    f.remove(fridgePane);
-                    f.add(cartPane);
-                }
-                else {
-                    f.remove(cartPane);
-                    f.add(fridgePane);
-                }
-                f.repaint();
-                f.revalidate();
-            }
-        });
+        // boolean cart = false;
+        // switchTabs.addActionListener(new ActionListener() {
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         if (cart) {
+        //             f.remove(fridgePane);
+        //             f.add(cartPane);
+        //         }
+        //         else {
+        //             f.remove(cartPane);
+        //             f.add(fridgePane);
+        //         }
+        //         f.repaint();
+        //         f.revalidate();
+        //     }
+        // });
     }
 }
 
